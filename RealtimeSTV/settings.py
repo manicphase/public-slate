a520f8f803fb6104451bab832667ebd7949d8560
manicphase <thegr8dave@gmail.com>
2024-06-29 17:47:58 +0100

    fix path for prod

--- RealtimeSTV/settings.py
+++ RealtimeSTV/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"RealtimeSTV/static"),
-    os.path.join(BASE_DIR, "AlternativeVote/static"),
+    os.path.join(BASE_DIR,"/RealtimeSTV/static"),
+    os.path.join(BASE_DIR, "/AlternativeVote/static"),
 ]
 
 # Default primary key field type
